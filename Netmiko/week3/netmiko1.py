cf626326ac9d62454a5c6fd6d46511357d1fb0d8
nathaniel-mills <nathaniel1984@hotmail.com>
2022-10-14 18:21:27 +0100

    master adding more week 3

--- Netmiko/week3/netmiko1.py
+++ Netmiko/week3/netmiko1.py
@@ -1,30 +1,18 @@
-my_list = [
-	{
-		'mac_addr': '0062.ec29.70fe',
-		'ip_addr': '10.220.88.1',
-		'interface': 'Gi0/0/0'
-	},
-	{
-		'mac_addr': 'c89c.1dea.0eb6',
-		'ip_addr': '10.220.88.20',
-		'interface': 'Gi0/0/0'
-	},
-	{
-		'mac_addr': 'a093.5141.b780',
-		'ip_addr': '10.220.88.22',
-		'interface': 'Gi0/0/0'
-	},
-	{
-		'mac_addr': '0001.00ff.0001',
-		'ip_addr': '10.220.88.37',
-		'interface': 'Gi0/0/0'
-	},
-	{
-		'mac_addr': '0002.00ff.0001',
-		'ip_addr': '10.220.88.38',
-		'interface': 'Gi0/0/0'
-	}
+from pprint import pprint
 
-]
+arp_data = """
+Protocol  Address      Age  Hardware Addr   Type  Interface
+Internet  10.220.88.1   67  0062.ec29.70fe  ARPA  Gi0/0/0
+Internet  10.220.88.20  29  c89c.1dea.0eb6  ARPA  Gi0/0/0
+Internet  10.220.88.22   -  a093.5141.b780  ARPA  Gi0/0/0
+Internet  10.220.88.37 104  0001.00ff.0001  ARPA  Gi0/0/0
+Internet  10.220.88.38 161  0002.00ff.0001  ARPA  Gi0/0/0
+"""
 
-print(my_list)
+arp_data = arp_data.strip()
+arp_list = arp_data.splitlines()
+
+new_list = []
+for arp in arp_list:
+	print(new_list)
+	break
--- Netmiko/week3/netmiko5.py
+++ Netmiko/week3/netmiko5.py
@@ -1,29 +1,21 @@
-from netmiko import ConnectHandler
-
-print ("show prompt from device")
-
-nxos1 = {
-	'device_type': 'cisco_nxos',
-	'host': 'nxos1.lasthop.io',
-	'username': 'pyclass',
-	'password': '88newclass',
-}
-
-nxos2 = {
-	'device_type': 'cisco_nxos',
-        'host': 'nxos2.lasthop.io',
-        'username': 'pyclass',
-        'password': '88newclass',
-}
-
-devices = [nxos1, nxos2]
-
-config_file = "vlan.txt"
+import yaml
+from yaml.loader import SafeLoader
 
 
-for kit in (devices):
-	net_connect = ConnectHandler(**kit)
-	output = net_connect.send_config_from_file(config_file)
-	print(output)
-	new_output = net_connect.save_config()
-	print(new_output)
+print ("Open the yaml file")
+
+with open('netmiko.yml') as f:
+	data = yaml.load(f, Loader=SafeLoader)
+print(data)
+
+print(type(data))
+
+print(data.keys())
+
+target_device = data['cisco3']
+
+print(target_device)
+
+net_connect = ConnectHandler(**target_device)
+
+print(net_connect.find_prompt())
